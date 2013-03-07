72288b1979041838b69f7768bed143545081592b
dartKwisatzHaderach <datonkushin@gmail.com>
2013-03-07 18:49:07 +0600

    add cities module

--- tests/run.rb
+++ tests/run.rb
@@ -1,9 +1,11 @@
 #encoding:utf-8
 
 require_relative '../config/proxy.rb'
+require_relative '../config/cities.rb'
 
 #Поиск прогноза погоды по городу
-system ("ruby select_city.rb Бора-Бора")
+system ("ruby select_city.rb #{Cities::BORA_BORA}")
+system ("ruby select_city.rb #{Cities::MOSCOW}")
 
 #Прогрноз погоды для разных городов
 system ("ruby check_city.rb #{Proxy::PROXY_MOSCOW}")
